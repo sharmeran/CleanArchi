d000af3446f5cc8a6904969d28790d48652bb65b
Hamzah Mahafzah <sharmeran@hotmail.com>
2019-11-14 14:56:26 +0300

    commit course model

--- CleanArchCore/CleanArchCore.DomainLayer/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace CleanArchCore.DomainLayer
-{
-    public class Class1
-    {
-    }
-}
---
+++ CleanArchCore/CleanArchCore.DomainLayer/Entities/CourseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchCore.DomainLayer.Entities
+{
+   public class CourseEntity
+    {
+        public int ID { set; get; }
+        public string Name { set; get; }
+        public string Description { set; get; }
+        public string ImageURL { set; get; }
+
+    }
+}
